4bab0ddb816b252a32ab5fefd70144e5f4bd12ab
Jannik1020 <jannik.thiele.official@gmail.com>
2020-04-28 22:00:02 +0200

    Updates

---
+++ precache-manifest.9589893ef4d3687fef521748989cc21e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825442b81077e61052241167f8bff44f",
+    "url": "/jannik1020.github.io/calculator/index.html"
+  },
+  {
+    "revision": "b820238847b4646c2291",
+    "url": "/jannik1020.github.io/calculator/static/css/main.e5721466.chunk.css"
+  },
+  {
+    "revision": "75a254e6ef2f33e4447d",
+    "url": "/jannik1020.github.io/calculator/static/js/2.8a35304c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jannik1020.github.io/calculator/static/js/2.8a35304c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b820238847b4646c2291",
+    "url": "/jannik1020.github.io/calculator/static/js/main.fd1326ee.chunk.js"
+  },
+  {
+    "revision": "40c6b0f4fe0aa014f274",
+    "url": "/jannik1020.github.io/calculator/static/js/runtime-main.1afdf33a.js"
+  }
+]);
